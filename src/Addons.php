59bb90d9318a8508ac4b782c7fcdcda9d70083c2
轻色年华 <13675626825@163.com>
2016-12-03 16:35:36 +0800

    add modelHook and viewHook

--- src/Addons.php
+++ src/Addons.php
@@ -7,9 +7,21 @@ abstract class Addons
 {
 
     /**
-     * 需要配置的数组
+     * 插件需要的配置
      */
     protected $_config = [];
+
+    /**
+     * 插件注册的业务钩子
+     * @var array
+     */
+    protected $modelHook = [];
+
+    /**
+     * 插件注册的视图钩子
+     * @var array
+     */
+    protected $viewHook = [];
 
     /** 安装方法 */
     public abstract function up();
