7a781d51e5b5c6215c966155d711d017f538f35a
轻色年华 <13675626825@163.com>
2016-12-08 13:25:08 +0800

    delete addons register hook on main file

--- src/Addons.php
+++ src/Addons.php
@@ -4,18 +4,6 @@ namespace Qsnh\think\Addons;
 abstract class Addons
 {
 
-    /**
-     * 插件注册的业务钩子
-     * @var array
-     */
-    protected $modelHook = [];
-
-    /**
-     * 插件注册的视图钩子
-     * @var array
-     */
-    protected $viewHook = [];
-
     /** 安装方法 */
     public abstract function up();
 
