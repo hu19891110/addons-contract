74f8597b6dbd8e3fd0c441ea2efd6dad3ec16bc2
轻色年华 <13675626825@163.com>
2016-12-08 14:14:36 +0800

    add output method in AddonsController

--- src/AddonsController.php
+++ src/AddonsController.php
@@ -19,4 +19,21 @@ class AddonsController extends Controller
 		throw new \Exception("The {$name} Methods does not exist.");
 	}
 
+	/** 
+	 * 视图输出渲染
+	 * @param  string $file 模板文件
+	 * @param  array  $data 渲染参数
+	 * @return think\View
+	 */
+    protected function output($file, $data = [])
+    {
+        $path = __DIR__ . DS . '..' . DS . 'view' . DS . $file . '.html';
+
+        if (!file_exists($path)) {
+            throw new \Exception("{$path} template file not exists.");
+        }
+
+        return $this->fetch($path, $data);
+    }
+
 }
